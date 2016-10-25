abe9fec514afedbbacbbd0dd62278841e79c248b
edwinmah <me@edwinmah.com>
2016-10-24 20:32:46 -0400

    Add mLab config

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
+process.env.NODE_ENV = 'production';
+
 exports.DATABASE_URL = process.env.DATABASE_URL ||
   global.DATABASE_URL ||
   (process.env.NODE_ENV === 'production' ?
-   'mongodb://localhost/node-lms' :
-   'mongodb://localhost/node-lms-dev');
+   'mongodb://emah:7egK6xGc6[av7a7FL7bVebwy/@ds031597.mlab.com:31597/node-lms' :
+   'mongodb://emah:7egK6xGc6[av7a7FL7bVebwy/@ds031597.mlab.com:31597/node-lms-dev');
 
 exports.PORT = process.env.PORT || 8080;
