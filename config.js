2d14e01d4596a9068e6c5cf626fb3523965dca54
edwinmah <me@edwinmah.com>
2016-10-25 19:33:46 -0400

    Add mLab again

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
+process.env.NODE_ENV = 'production';
+
 exports.DATABASE_URL = process.env.DATABASE_URL ||
   global.DATABASE_URL ||
   (process.env.NODE_ENV === 'production' ?
-   'mongodb://localhost/node-lms' :
-   'mongodb://localhost/node-lms-dev');
+   'mongodb://emah:Cx7drCVaLFvGnuiFGT7Mud4R9VJv@ds031597.mlab.com:31597/node_lms' :
+   'mongodb://emah:Cx7drCVaLFvGnuiFGT7Mud4R9VJv@ds031597.mlab.com:31597/node_lms-dev');
 
 exports.PORT = process.env.PORT || 8080;
