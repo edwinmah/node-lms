bad8cb198c948529d5c344bb2c334c72c7563525
edwinmah <me@edwinmah.com>
2016-10-26 13:32:05 -0400

    Reset mongo database URL

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ process.env.NODE_ENV = 'production';
 exports.DATABASE_URL = process.env.DATABASE_URL ||
   global.DATABASE_URL ||
   (process.env.NODE_ENV === 'production' ?
-   'mongodb://emah:Cx7drCVaLFvGnuiFGT7Mud4R9VJv@ds031597.mlab.com:31597/node_lms' :
-   'mongodb://emah:Cx7drCVaLFvGnuiFGT7Mud4R9VJv@ds031597.mlab.com:31597/node_lms-dev');
+   'mongodb://localhost/node-lms' :
+   'mongodb://localhost/node-lms-dev');
 
 exports.PORT = process.env.PORT || 8080;
