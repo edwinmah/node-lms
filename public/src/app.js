93db77b0921617b669b6b4c0ea9f08836b2f48ed
edwinmah <me@edwinmah.com>
2016-10-29 21:29:14 -0400

    Add missing semicolon

--- public/src/app.js
+++ public/src/app.js
@@ -11,7 +11,7 @@ require('../src/style.css');
 var ICONS = {
   edit   : '<svg class="icon icon-edit" aria-labelledby="title desc" role="img"><use xlink:href="#icon-edit"></use></svg>',
   delete : '<svg class="icon icon-trash" aria-labelledby="title desc" role="img"><use xlink:href="#icon-trash"></use></svg>'
-}
+};
 
 var md = new MarkdownIt({
   linkify: true,
