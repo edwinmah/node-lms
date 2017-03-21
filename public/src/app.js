cbe5228c038bc97b32acabe3ddafa7e19c834b52
edwinmah <me@edwinmah.com>
2017-03-21 15:26:00 -0400

    Remove unneeded Mag popup option

--- public/src/app.js
+++ public/src/app.js
@@ -19,7 +19,6 @@ $(function () {
   $('.popup-modal').magnificPopup({
     type: 'inline',
     preloader: false,
-    focus: '#username',
     modal: true
   });
   $(document).on('click', '.popup-modal-dismiss', function (e) {
